81fc0790a3331a0ca64614e164f619e3820e1f36
Victor Tavares <victor.as156@gmail.com>
2020-11-26 13:31:26 -0300

    Estruturando página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,10 @@
-let nome = document.querySelector('#nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
-let nomeOk = false
 let emailOk = false
 let assuntoOk = false
-let mapa = document.querySelector("#mapa")
 
-nome.style.width = '100%'
 email.style.width = '100%'
 
-function validaNome(){
-    let txtNome = document.querySelector('#txtNome')
-    if(nome.value.length < 3){
-        txtNome.innerHTML = 'Nome inválido!'
-        txtNome.style.color = 'red'
-    } else {
-        txtNome.innerHTML = 'Nome válido!'
-        txtNome.style.color = 'green'
-        nomeOk = true
-    }
-}
 
 function validaEmail(){
     let txtEmail = document.querySelector('#txtEmail')
@@ -36,8 +21,8 @@ function validaEmail(){
 
 function validaAssunto(){
     let txtAssunto = document.querySelector('#txtAssunto')
-    if(assunto.value.length >= 100){
-        txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 100 caracteres!'
+    if(assunto.value.length >= 1000){
+        txtAssunto.innerHTML = 'Texto muito grande, digite no máximo 1000 caracteres!'
         txtAssunto.style.color = 'red'
         txtAssunto.style.display = 'block'
     } else {
@@ -47,19 +32,9 @@ function validaAssunto(){
 }
 
 function enviar(){
-    if(nomeOk == true && emailOk == true && assuntoOk == true){
+    if(emailOk == true && assuntoOk == true){
         alert ('Formulário enviado com sucesso!')
     } else {
         alert ('Preencha o formulário corretamente antes de enviar!')
     }
-}
-
-function mapaZoom(){
-    mapa.style.width = '800px'
-    mapa.style.height = '600px'
-}
-
-function mapaNormal(){
-    mapa.style.width="510px" 
-    mapa.style.height="330px"
 }
